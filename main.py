7e3fdc2efefbc21bf99450c9915cd7ec47d04c99
partiadam <71691758+partiadam@users.noreply.github.com>
2022-01-24 10:00:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+1. Feladat
+Írj egy programot while ciklussal amely kiírja a páros számokat 1 és 10 között!
+'''
+
+x = 0
+
+while x < 10:
+    if x %2 == 0:
+        print(x)
+    x += 1
+       
